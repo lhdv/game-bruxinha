0e7045c505caa27a24bd42d50f742a07329c2a55
Leandro Hoffman <lhoffman@metalbook.local>
2020-06-23 21:52:37 -0300

    Cria propriedade de velocidade para movimentacao do inimigo

--- scripts/jogo/inimigo.js
+++ scripts/jogo/inimigo.js
@@ -1,10 +1,12 @@
 class Inimigo extends Animacao {
     constructor(mtzLin, mtzCol, imagem, x, largura, altura, altSprite, largSprite) {
         super(mtzLin, mtzCol, imagem, x, largura, altura, altSprite, largSprite);
+
+        this.velocidade = 8;
     }    
 
     move() {
-        this.x = this.x - 8;
+        this.x = this.x - this.velocidade;
         if (this.x < -this.largura) {
             this.x = width;
         }
