21b28a7903a89c947e9e74264a7ca964f2495cfd
Leandro Hoffman <lhoffman@metalbook.local>
2020-06-26 21:07:43 -0300

    Cria logica para mapa do jogo
    - Mapa define quais inimigos apareceram e as suas velocidades
    - Criado novo metodo para inimigo reaparecer em tela
    - Há necessidade de se ajustar a movimentacao dos personagens
    retirando o delay de posicao

--- scripts/cenas/jogo.js
+++ scripts/cenas/jogo.js
@@ -1,6 +1,24 @@
 class Jogo {
     constructor() {
-        this.inimigoAtual = 0;
+        this.indice = 0;
+        this.mapa = [
+          {
+            inimigo: 0,
+            velocidade: 10
+          },
+          {
+            inimigo: 2,
+            velocidade: 30
+          },
+          {
+            inimigo: 2,
+            velocidade: 20
+          },
+          {
+            inimigo: 1,
+            velocidade: 40
+          }
+        ];
     }
 
     setup() {
@@ -11,9 +29,9 @@ class Jogo {
       npc = new NonPlayerCharacter(1, 1, 0, imagemAbelha, width - 39.8, 300, 39.8, 35.4, 398, 354, 9);
       moeda = new Item(1, 8, 0, imagemMoeda, width - 32, 200, 32, 32, 16, 16, 9, somMoeda);
 
-      const inimigo = new Inimigo(7, 4, 0, imagemInimigo, width, 20, 52, 52, 104, 104, 8, 10);
-      const inimigoVoador = new Inimigo(6, 3, 2, imagemInimigoVoador, width, 200, 100, 75, 200, 150, 6, 10);
-      const inimigoGrande = new Inimigo(6, 5, 2, imagemInimigoGrande, width, 0, 150, 150, 400, 400, 6, 10);
+      const inimigo = new Inimigo(7, 4, 0, imagemInimigo, width, 20, 52, 52, 104, 104, 8);
+      const inimigoVoador = new Inimigo(6, 3, 2, imagemInimigoVoador, width, 200, 100, 75, 200, 150, 6);
+      const inimigoGrande = new Inimigo(6, 5, 2, imagemInimigoGrande, width, 0, 150, 150, 400, 400, 6);
 
       inimigos.push(inimigo);
       inimigos.push(inimigoVoador);
@@ -38,55 +56,57 @@ class Jogo {
     }
 
     draw() {
-        const inimigo = inimigos[this.inimigoAtual];
-        const inimigoVisivel = inimigo.x < (- inimigo.largura);
-      
-        cenario.exibe(); 
-        cenario.move();
+      const linhaAtual = this.mapa[this.indice];
+      const inimigo = inimigos[linhaAtual.inimigo];
+      const inimigoVisivel = inimigo.x < (- inimigo.largura);
 
-        vida.draw();
-      
-        pontuacao.exibe();
-        pontuacao.adicionaPonto();
-        
-        // npc.exibe();
-        // npc.move();
-      
-        moeda.exibe();
-        moeda.move();
-      
-        personagem.exibe();
-        personagem.aplicaGravidade();
-        
-        inimigo.exibe();
-        inimigo.move();
-      
-        if (inimigoVisivel) {
-          this.inimigoAtual++;
-          if (this.inimigoAtual > inimigos.length -1) {
-            this.inimigoAtual = 0;
-          }
-          inimigo.velocidade = parseInt(random(5,15));    
+      cenario.exibe();
+      cenario.move();
+
+      vida.draw();
+
+      pontuacao.exibe();
+      pontuacao.adicionaPonto();
+
+      // npc.exibe();
+      // npc.move();
+
+      moeda.exibe();
+      moeda.move();
+
+      personagem.exibe();
+      personagem.aplicaGravidade();
+
+      inimigo.velocidade = linhaAtual.velocidade;
+      inimigo.exibe();
+      inimigo.move();
+
+      if (inimigoVisivel) {
+        this.indice++;
+        inimigo.aparece();
+        if (this.indice > this.mapa.length - 1) {
+          this.indice = 0;
         }
-      
-        if (personagem.estaColidindo(inimigo)) {                  
-          vida.perdeVida();
-          personagem.tornarInvencivel();
-          
-          if (vida.vidas === 0) {
-            somJogo.setVolume(0, 0.4);
-            noLoop();
-            image(imageGameOver, (width / 2) - (imageGameOver.width / 2), height / 2);
-            somGameOver.play();
-            somGameOver.setVolume(0.5)
-          }
-          
+      }
+
+      if (personagem.estaColidindo(inimigo)) {
+        vida.perdeVida();
+        personagem.tornarInvencivel();
+
+        if (vida.vidas === 0) {
+          somJogo.setVolume(0, 0.4);
+          noLoop();
+          image(imageGameOver, (width / 2) - (imageGameOver.width / 2), height / 2);
+          somGameOver.play();
+          somGameOver.setVolume(0.5)
         }
-      
-        if (personagem.estaColidindo(moeda)) {
-          moeda.tocaSom();
-          moeda.x = width * 2;
-          pontuacao.adicionaMoeda();
-        }
+
+      }
+
+      if (personagem.estaColidindo(moeda)) {
+        moeda.tocaSom();
+        moeda.x = width * 2;
+        pontuacao.adicionaMoeda();
+      }
     }
 }
--- scripts/jogo/inimigo.js
+++ scripts/jogo/inimigo.js
@@ -1,15 +1,15 @@
 class Inimigo extends Animacao {
-    constructor(mtzLin, mtzCol, mtzTrim, imagem, x, variacaoY, largura, altura, largSprite, altSprite, velocidade, delay) {
+    constructor(mtzLin, mtzCol, mtzTrim, imagem, x, variacaoY, largura, altura, largSprite, altSprite, velocidade) {
         super(mtzLin, mtzCol, mtzTrim, imagem, x, variacaoY, largura, altura, largSprite, altSprite);
 
         this.velocidade = velocidade;
-        this.delay = delay;    
     }    
 
     move() {
         this.x = this.x - this.velocidade;
-        if (this.x < -this.largura - this.delay) {
-            this.x = width;
-        }
+    }
+    
+    aparece() {
+        this.x = width;
     }
 }
